e74c8092706a60d95bc04f07054d121e6a3c3bd3
OmarKhaled192 <okoros192000@gmail.com>
2026-05-16 10:35:34 +0300

    update-config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,31 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { MessageService } from 'primeng/api';
 import { routes } from './app.routes';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideZoneChangeDetection({
+      eventCoalescing: true,
+    }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
+    provideHttpClient(
+      withInterceptors([
+        errorInterceptor,
+      ])
+    ),
+    MessageService,
+  ],
 };
