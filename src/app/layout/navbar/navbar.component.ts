ca41dcde05935762efbc0e8cb34779f7034e48b5
OmarKhaled192 <okoros192000@gmail.com>
2026-05-17 11:27:21 +0300

    update

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../../core/services/auth.service';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
-
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -16,6 +16,18 @@ import { MenuModule } from 'primeng/menu';
 export class NavbarComponent {
   public readonly auth = inject(AuthService);
   protected readonly userName = this.auth.currentUserName;
+
+  menuItems: MenuItem[] = [
+    {
+      label: 'Profile',
+      icon: 'pi pi-user'
+    },
+    {
+      label: 'Sign Out',
+      icon: 'pi pi-sign-out',
+      command: () => this.logout()
+    }
+  ];
 
   logout(): void {
     this.auth.logout();
